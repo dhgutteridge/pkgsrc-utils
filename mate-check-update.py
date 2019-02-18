658e92ac62c084bc8965f8b038d1b003f21368b6
Youri Mouton <youri@NetBSD.org>
2019-02-18 02:50:06 +0100

    it works

--- mate-check-update.py
+++ mate-check-update.py
@@ -23,43 +23,33 @@ from distutils.version import StrictVersion
 #                                                   #
 #####################################################
 
-version = 1.21
 base_url = 'http://pub.mate-desktop.org/releases/'
+extension = '.tar.xz'
 
 def get_links(link):
-        pars = BeautifulSoup(urllib.request.urlopen(base_url+link),
-                features='lxml')
+        req = urllib.request.Request(base_url+link,
+            headers={'User-Agent' : "Magic Browser"})
+        pars = BeautifulSoup(urllib.request.urlopen(req), features='lxml')
         link = pars.body.find('table').find_all('a', href=True)
         return link
     
-def filter_links(links):
+def get_most_recent(link, name):
+        links = get_links(link)
         l = list(filter(lambda x: 
-            x['href'] not in ['/src/', '?C=N;O=D', '?C=M;O=A', '?C=S;O=A']
-            and x.text != ''
-            and x.text != 'Parent Directory', links))
+            x['href'] not in ['?C=N&O=D', '?C=S&O=D', '?C=M&O=D', '?C=N&O=D',
+                '?C=N&O=A', '?C=M&O=A', '?C=S&O=A', 'SHA1SUMS', '../'] and
+            x.text.startswith(name) and
+            x.text.count('-') - 1 == name.count('-'), links))
         l.sort(key=lambda x: x.text)
-        return l
+        if len(l) == 0:
+            return ''
+        return l[-1].text.replace(extension, '').replace(name + '-', '')
 
-def get_sublinks(link):
-        links = filter_links(get_links(link))
-        sublinks = []
-        for l in links:
-            sublinks.append(link + l['href'])
-        return sublinks
-
-def get_upstream_version(project):
-        #project_versions = get_sublinks(project)
-
-        # sort by project version
-        #pv.sort(key=lambda x: StrictVersion(x.split("/")[2]))
-
-        #try: last_version = pv[-1]
-        #except IndexError: return # empty project
-
-        #files_versions = get_sublinks(last_version)
-        #return files_versions[-1]
-        s = get_sublinks(base_url + version + '/')
-        print(s)
+def get_upstream_version(v, project):
+        s = get_most_recent('1.' + str(v) + '/', project)
+        if s == '':
+            s = get_upstream_version(v-1, project)
+        return s
         
 
 #####################################################
@@ -100,7 +90,6 @@ packages = ["atril",
         "mate-settings-daemon",
         "mate-system-monitor",
         "mate-terminal",
-        "mate-themes",
         "mate-user-share",
         "mate-utils",
         "mozo",
@@ -121,7 +110,7 @@ def get_package_version(name):
         version = pars2.body.find('b').text[8:end]
         return version
 
-def get_packages_pkgsrc_version():
+def get_pkgsrc_version():
         versions = {}
         for name in packages:
             versions[name] = get_package_version(name)
@@ -135,34 +124,15 @@ def get_packages_pkgsrc_version():
 #                                                   #
 #####################################################
 
-print('Checking upstream versions...', file=sys.stderr)
-#upstream = get_upstream_versions()
-#upstream = {'xfwm4': 'url/xfwm4-4.4.4.tar.gz',
-#            'catfish': 'url/catfish-1.0.0.tar.gz',
-#            'xfce4-screenshooter': 'url/xfce4-screenshooter-10.0.tar.bz2',
-#            'orage': 'url/orage-4.tar.gz'}
-print('Checking local pkgrsc versions...', file=sys.stderr)
 print('package name , pkgsrc version , upstream version , needs update')
-#versions = get_packages_pkgsrc_version()
-#print(versions)
-
-#for name,version in versions.items():
-#   print(name + ' ' + version)
-up_version = get_upstream_version('eom')
-
-#    if up is not None:
-#        local = get_package_version(name)
-#        if local == '':
-#            print(name + ' , / , / , /')
-#        else:
-#            # extract versions, we expect two extension archives
-#            lv = re.sub(r'nb[0-9]+', ' ', local).strip()
-#            tv = up.split('/')[-1].split('-')[-1].split('.')
-#            uv = '.'.join(tv[:len(tv)-2])
-#            try : needsupdated = StrictVersion(lv) < StrictVersion(uv)
-#            except: needsupdated = False
-#            print(name + ' , ' + lv + ' , ' + uv + ' , ' +
-#                    ('yes' if needsupdated else 'no') + ',')
-#    else:
-#        print(name + ' , / , / , /')
+versions = get_pkgsrc_version()
+for name,uv in versions.items():
+    lv = get_upstream_version(21, name)
+    # extract versions, we expect two extension archives
+    lv = re.sub(r'nb[0-9]+', ' ', lv).strip()
+    try : needsupdated = StrictVersion(lv) < StrictVersion(uv)
+    except: needsupdated = False
+    print(name + ' , ' + lv + ' , ' + uv + ' , ' +
+            ('yes' if needsupdated else 'no'))
 print('Done...', file=sys.stderr)
+print('Dont forget to check mate-themes: http://pub.mate-desktop.org/releases/themes/, pkgsrc version is ' + get_package_version('mate-theme'), file=sys.stderr)
